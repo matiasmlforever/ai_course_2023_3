053e3e5955570a2b08103446f28bd87e70aa9d3e
Matias Lecaros <m.lecaros.aranis@gmail.com>
2023-03-01 01:15:55 -0300

    Files created and relocated

---
+++ model/Agente.py
@@ -0,0 +1,3 @@
+class Agente:
+    def __init__(self, id, ):
+
---
+++ model/Ambiente.py
@@ -0,0 +1,5 @@
+class Ambiente:
+    def __init__(self, size, timestamp):
+        self.size = size
+        self.timestamp = timestamp
+
